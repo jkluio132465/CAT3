eecb1e4c97266177bb0ae35653639e715848e1f4
jkluio132465 <jkluio132465@gmail.com>
2017-11-27 01:56:32 +0800

    layouts

---
+++ app/src/main/java/com/example/anew/cat/Tab1.java
@@ -0,0 +1,21 @@
+package com.example.anew.cat;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by NEW on 2017/11/24.
+ */
+
+public class Tab1 extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tab1,container,false);
+        return v;
+    }
+}
